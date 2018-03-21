54d1f1abcdcee0e634d8bbfeeb40771e7f4cc26c
Glaeqen <glaeken@vp.pl>
2018-03-21 23:55:43 +0100

    Changes from Robert applied applied IntelliJ code style & optimised imports

--- src/main/java/pl/edu/agh/fis/anistratenko_team_project/Main.java
+++ src/main/java/pl/edu/agh/fis/anistratenko_team_project/Main.java
@@ -1,36 +1,125 @@
 package pl.edu.agh.fis.anistratenko_team_project;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 
+class Point {
+    private double x;
+    private double y;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
+
+class PendulumConfig {
+    public Circle circ1, circ2;
+    public Line l1, l2;
+
+    public PendulumConfig(Circle circ1, Circle circ2, Point start) {
+        this.circ1 = circ1;
+        this.circ2 = circ2;
+        setLines(start);
+    }
+
+    private void setLines(Point start) {
+        l1 = new Line(start.getX(), start.getY(), circ1.getCenterX(), circ1.getCenterY());
+        l2 = new Line(circ1.getCenterX(), circ1.getCenterY(), circ2.getCenterX(), circ2.getCenterY());
+    }
+}
+
 public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                System.out.println("World!");
-            }
+
+        Circle circ1 = new Circle(300.f, 270.f, 10.f);
+        Circle circ2 = new Circle(400.f, 270.f, 5.f);
+
+        PendulumConfig config = new PendulumConfig(circ1, circ2, new Point(250, 250));
+
+        //mass dependend from radius, l1, l2 will be overwritten in setXY
+        Pendulum P1 = new Pendulum(0.25, circ1.getRadius() / 10., 0.25, circ2.getRadius() / 10);
+        //locations based on circles
+        P1.setXY((circ1.getCenterX() - 250.) / 250., (circ1.getCenterY() - 250.) / 250., (circ2.getCenterX() - 250.) / 250., (circ2.getCenterY() - 250.) / 250.);
+
+        runAnimation(config.circ1, config.circ2, config.l1, config.l2, P1);
+
+        setWindow(primaryStage, config.circ1, config.circ2, config.l1, config.l2);
+    }
+
+    private void setWindow(Stage primaryStage, Circle circ1, Circle circ2, Line l1, Line l2) {
+        primaryStage.setTitle("Double");
+        Polygon polygon = new Polygon();
+        polygon.getPoints().addAll(new Double[]{
+                250., 250.,
+                265., 265.,
+                235., 265.
         });
 
+        polygon.setFill(Color.TRANSPARENT);
+        polygon.setStroke(Color.BLUE);
+        polygon.setStrokeType(StrokeType.OUTSIDE);
+        polygon.setStrokeWidth(4.);
+        polygon.setStrokeLineJoin(StrokeLineJoin.ROUND);
+
+
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        root.getChildren().add(new Pane(polygon));
+        root.getChildren().add(new Pane(circ1));
+        root.getChildren().add(new Pane(circ2));
+        root.getChildren().add(new Pane(l1));
+        root.getChildren().add(new Pane(l2));
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
-    public static void main(String[] args) {
-        launch(args);
+
+    private void runAnimation(Circle circ1, Circle circ2, Line l1, Line l2, Pendulum p1) {
+        new AnimationTimer() {
+            long lastUpdate = 0;
+
+            public void handle(long now) {
+                if (now - lastUpdate >= 17_000_000) {
+                    p1.simulate(1. / 60.);
+                    circ1.setCenterX(250 + p1.x1 * 250);
+                    circ1.setCenterY(250 - p1.y1 * 250);
+                    circ2.setCenterX(250 + p1.x2 * 250);
+                    circ2.setCenterY(250 - p1.y2 * 250);
+                    l1.setEndX(250 + p1.x1 * 250);
+                    l1.setEndY(250 - p1.y1 * 250);
+                    l2.setStartX(250 + p1.x1 * 250);
+                    l2.setStartY(250 - p1.y1 * 250);
+                    l2.setEndX(250 + p1.x2 * 250);
+                    l2.setEndY(250 - p1.y2 * 250);
+                    lastUpdate = now;
+                }
+            }
+        }.start();
     }
 }
--- src/main/java/pl/edu/agh/fis/anistratenko_team_project/Pendulum.java
+++ src/main/java/pl/edu/agh/fis/anistratenko_team_project/Pendulum.java
@@ -1,161 +1,151 @@
 package pl.edu.agh.fis.anistratenko_team_project;
-public class Pendulum 
-{
-	private final double PI = 3.14159265359;
-	//variables for simulation
-	private double phi = PI/2;
-	private double theta = 0.;
-	private double d_phi = 0.;
-	private double d_theta = 0.;
-	private double d2_phi = 0.;
-	private double d2_theta = 0.;
-	private double l1 = 0.;
-	private double l2 = 0.;
-	private double m1 = 0.;
-	private double m2 = 0.;
-	private double g = 9.81;
-	private double real_t = 0.;
-	private double t = 0.;
 
-	//flag for double/single pendulum distinction
-	private boolean DP;
+public class Pendulum {
+    private final double PI = 3.14159265359;
+    //variables for simulation
+    private double phi = PI / 2;
+    private double theta = 0.;
+    private double d_phi = 0.;
+    private double d_theta = 0.;
+    private double d2_phi = 0.;
+    private double d2_theta = 0.;
+    private double l1 = 0.;
+    private double l2 = 0.;
+    private double m1 = 0.;
+    private double m2 = 0.;
+    private double g = 9.81;
+    private double real_t = 0.;
+    private double t = 0.;
 
-	//public variables x1, y1 - coordinates of 1st pendulum end, x2, y2 - 2nd pendulum or x2==x1, y2==y1 if DP equals false
-	//coordinate system origin is located in pendulum pivot
-	//modification of these values won't affect simulate function
-	public double x1;
-	public double y1;
-	public double x2;
-	public double y2;
+    //flag for double/single pendulum distinction
+    private boolean DP;
 
-	public void setXY(double X1, double Y1, double X2, double Y2)
-	{
-		l1 = Math.sqrt(X1*X1 + Y1*Y1);
-		phi = Math.acos(X1/l1);
-		if (Y1 >= 0){ phi += PI/2.; }
-		else 		{ phi =  PI/2. - phi; }
-		
-		if (DP)
-		{
-			l2 = Math.sqrt((X2-X1)*(X2-X1) + (Y2-Y1)*(Y2-Y1));
-			theta = Math.acos(X1/l1);
-			if (Y2 >= Y1){ theta += PI/2.; }
-			else 		 { theta =  PI/2. - theta; } 
-			d_theta = 0;
-			d2_theta = 0;
-		}
-		
-		d_phi = 0;
-		d2_phi = 0;
-		real_t = 0.;
-		t = 0.;
-	}
+    //public variables x1, y1 - coordinates of 1st pendulum end, x2, y2 - 2nd pendulum or x2==x1, y2==y1 if DP equals false
+    //coordinate system origin is located in pendulum pivot
+    //modification of these values won't affect simulate function
+    public double x1;
+    public double y1;
+    public double x2;
+    public double y2;
 
-	public void setG(double G)
-	{
-		g = G;
-	}
+    public void setXY(double X1, double Y1, double X2, double Y2) {
+        l1 = Math.sqrt(X1 * X1 + Y1 * Y1);
+        phi = Math.acos(X1 / l1);
+        if (Y1 >= 0) {
+            phi += PI / 2.;
+        } else {
+            phi = PI / 2. - phi;
+        }
 
-	//constructor for single pendulum
-	public Pendulum(double l_1, double m_1)
-	{
-		this(l_1, m_1, 10, 0);
-		DP = false;
-	}
+        if (DP) {
+            l2 = Math.sqrt((X2 - X1) * (X2 - X1) + (Y2 - Y1) * (Y2 - Y1));
+            theta = Math.acos(X1 / l1);
+            if (Y2 >= Y1) {
+                theta += PI / 2.;
+            } else {
+                theta = PI / 2. - theta;
+            }
+            d_theta = 0;
+            d2_theta = 0;
+        }
 
-	//constructor for double pendulum
-	public Pendulum(double l_1, double m_1, double l_2, double m_2)
-	{
-		DP = true;
-		l1 = l_1;
-		m1 = m_1;
-		l2 = l_2;
-		m2 = m_2;
-	}
+        d_phi = 0;
+        d2_phi = 0;
+        real_t = 0.;
+        t = 0.;
+    }
 
-	//function for standard simulation, time >= 0
-	public void simulate(double time)
-	{
-		simulate(time, 0);
-	}
+    public void setG(double G) {
+        g = G;
+    }
 
-	//function for simulation with additional horizontal force, works only for single pendulum, time >= 0, F [m/s^2] in (-inf;inf)
-	public void simulate(double time, double F)
-	{
-		//internal time-step for computations
-		double dt = 5e-4;
-		//time measuring
-		real_t += time;
-		if (DP)
-		{
-			while (t < real_t)
-			{
-				d2_phi = 	(
-							 - m2*l1*d_theta*d_theta*Math.sin(phi-theta)*Math.cos(phi-theta) 
-							 + g*m2*Math.sin(theta)*Math.cos(phi-theta)
-							 - m2*l2*d_theta*d_theta*Math.sin(phi-theta)
-							 - (m1+m2)*g*Math.sin(phi) 
-							) 
-							/ 
-							(
-							  l1*(m1+m2) - m2*l1*Math.cos(phi-theta)*Math.cos(phi-theta)
-							);
-				
-				d2_theta =	(  m2*l2*d_theta*d_theta*Math.sin(phi-theta)*Math.cos(phi-theta) 
-							 + g*Math.sin(phi)*Math.cos(phi-theta)*(m1+m2)
-							 + l1*d_phi*d_phi*Math.sin(phi-theta)*(m1+m2)
-							 - g*Math.sin(theta)*(m1+m2)
-							)
-							/
-							(
-							  l2*(m1+m2) - m2*l2*Math.cos(phi-theta)*Math.cos(phi-theta)
-							);
-				
-				d_phi += d2_phi*dt;
-				d_theta += d2_theta*dt;
-				phi += d_phi*dt;
-				theta += d_theta*dt;
-				t += dt;
-			}
-		}
-		else 
-		{
-			while (t < time)
-			{
-				//System.out.println(phi + " " + theta);
-				
-				d2_phi 	=  (-g * Math.sin(phi) + F*Math.cos(phi))/l1;
-				d_phi 	+= d2_phi*dt;
-				phi 	+= d_phi*dt;
-				t 		+= dt;
-			}
-		}
-		definePositions();
-	}
+    //constructor for single pendulum
+    public Pendulum(double l_1, double m_1) {
+        this(l_1, m_1, 10, 0);
+        DP = false;
+    }
 
-	private void definePositions()
-	{
-		x1 = l1 * Math.sin(phi);
-		y1 = -l1 * Math.cos(phi);
-		if (DP)
-		{
-			x2 = x1 + l2*Math.sin(theta);
-			y2 = y1 - l2*Math.cos(theta); 
-		}
-		else 
-		{
-			x2 = x1;
-			y2 = y1;
-		}
-	}
+    //constructor for double pendulum
+    public Pendulum(double l_1, double m_1, double l_2, double m_2) {
+        DP = true;
+        l1 = l_1;
+        m1 = m_1;
+        l2 = l_2;
+        m2 = m_2;
+    }
 
-	public static void main(String[] args) {
-		Pendulum p = new Pendulum(0.25, 1, 0.3,2);
-		
-		Long start = System.nanoTime();
-		for (int i = 0; i < 600; i++) p.simulate(1./60.);
-		
-		System.out.println( "Computation time per 1 second simulated: " + ((System.nanoTime() - start)*1e-9/10) );
-		System.out.println( "Computation time of 1/60 sec: 			  " + ((System.nanoTime() - start)*1e-9/600));
-	}
+    //function for standard simulation, time >= 0
+    public void simulate(double time) {
+        simulate(time, 0);
+    }
+
+    //function for simulation with additional horizontal force, works only for single pendulum, time >= 0, F [m/s^2] in (-inf;inf)
+    public void simulate(double time, double F) {
+        //internal time-step for computations
+        double dt = 5e-4;
+        //time measuring
+        real_t += time;
+        if (DP) {
+            while (t < real_t) {
+                d2_phi = (
+                        -m2 * l1 * d_theta * d_theta * Math.sin(phi - theta) * Math.cos(phi - theta)
+                                + g * m2 * Math.sin(theta) * Math.cos(phi - theta)
+                                - m2 * l2 * d_theta * d_theta * Math.sin(phi - theta)
+                                - (m1 + m2) * g * Math.sin(phi)
+                )
+                        /
+                        (
+                                l1 * (m1 + m2) - m2 * l1 * Math.cos(phi - theta) * Math.cos(phi - theta)
+                        );
+
+                d2_theta = (m2 * l2 * d_theta * d_theta * Math.sin(phi - theta) * Math.cos(phi - theta)
+                        + g * Math.sin(phi) * Math.cos(phi - theta) * (m1 + m2)
+                        + l1 * d_phi * d_phi * Math.sin(phi - theta) * (m1 + m2)
+                        - g * Math.sin(theta) * (m1 + m2)
+                )
+                        /
+                        (
+                                l2 * (m1 + m2) - m2 * l2 * Math.cos(phi - theta) * Math.cos(phi - theta)
+                        );
+
+                d_phi += d2_phi * dt;
+                d_theta += d2_theta * dt;
+                phi += d_phi * dt;
+                theta += d_theta * dt;
+                t += dt;
+            }
+        } else {
+            while (t < time) {
+                //System.out.println(phi + " " + theta);
+
+                d2_phi = (-g * Math.sin(phi) + F * Math.cos(phi)) / l1;
+                d_phi += d2_phi * dt;
+                phi += d_phi * dt;
+                t += dt;
+            }
+        }
+        definePositions();
+    }
+
+    private void definePositions() {
+        x1 = l1 * Math.sin(phi);
+        y1 = -l1 * Math.cos(phi);
+        if (DP) {
+            x2 = x1 + l2 * Math.sin(theta);
+            y2 = y1 - l2 * Math.cos(theta);
+        } else {
+            x2 = x1;
+            y2 = y1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Pendulum p = new Pendulum(0.25, 1, 0.3, 2);
+
+        Long start = System.nanoTime();
+        for (int i = 0; i < 600; i++) p.simulate(1. / 60.);
+
+        System.out.println("Computation time per 1 second simulated: " + ((System.nanoTime() - start) * 1e-9 / 10));
+        System.out.println("Computation time of 1/60 sec: 			  " + ((System.nanoTime() - start) * 1e-9 / 600));
+    }
 }
