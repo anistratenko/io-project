904544a88df1cf85a0642d5ad353d0b019b971d2
Robert Gałat <robgal519@gmail.com>
2018-03-23 21:14:04 +0100

    change name to more apropriate

--- src/main/java/pl/edu/agh/fis/anistratenko_team_project/Draw.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.fis.anistratenko_team_project;
-
-import javafx.scene.layout.StackPane;
-
-public class Draw {
-    public StackPane GetPane(){}
-
-
-}
---
+++ src/main/java/pl/edu/agh/fis/anistratenko_team_project/SimulationPanel.java
@@ -0,0 +1,15 @@
+package pl.edu.agh.fis.anistratenko_team_project;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+public class SimulationPanel {
+    private Scene sceneToDraw;
+    private firstInit(StackPane parentPanel, int sizeX, int sizeY){
+        sceneToDraw = new Scene(parentPanel, sizeX,sizeY);
+    }
+    public Draw(){}
+    public StackPane GetScene(){}
+
+
+}
