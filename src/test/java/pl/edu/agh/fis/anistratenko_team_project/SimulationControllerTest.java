0ba72e718de6a2eb9579ecaa6e86d23cd1bc047b
Robert Gałat <robgal519@gmail.com>
2018-04-15 15:34:52 +0200

    toString tested

--- src/test/java/pl/edu/agh/fis/anistratenko_team_project/SimulationControllerTest.java
+++ src/test/java/pl/edu/agh/fis/anistratenko_team_project/SimulationControllerTest.java
@@ -6,6 +6,7 @@ import javafx.scene.shape.Circle;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -18,7 +19,7 @@ public class SimulationControllerTest {
         SimulationController simulation = new SimulationController(new Pane());
         simulation.addToPane(element);
         List<Node> elements = simulation.getSimulationPane().getChildren();
-        
+
         assertEquals("size of elements is 1",1, elements.size());
         assertTrue("Elements are the same", elements.get(0).equals(element));
     }
@@ -33,13 +34,24 @@ public class SimulationControllerTest {
         assertEquals(true, true);
     }
 
-    @Test @Ignore
-    public void getSimulationPane() {
-        assertEquals(true, true);
-    }
 
-    @Test @Ignore
-    public void to_String() {
-        assertEquals(true, true);
+    @Test
+    public void to_String(){
+        SimulationController simulation = new SimulationController(new Pane());
+        simulation.applySimulation(new SimulationView() {
+            @Override
+            public ArrayList<Node> getNodes() {
+                return new ArrayList<Node>();
+            }
+
+            @Override
+            public void performSimulationStep() {}
+
+            @Override
+            public String toString() {
+                return "test";
+            }
+        });
+        assertTrue("name is correct", simulation.toString().equals("test"));
     }
 }
